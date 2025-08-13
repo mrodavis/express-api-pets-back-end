4f8eea4e280a14d89c08bfbebdb6857e7f641b40
mrodavis <orville.davis@scrapit.io>
2025-08-13 12:13:10 -0400

    get functionality

--- controllers/pets.js
+++ controllers/pets.js
@@ -11,4 +11,34 @@ router.post('/', async (req, res) => {
     }
 });
 
+// READ - GET /pets
+router.get ('/', async (req, res) => {
+    try {
+        const foundPets = await Pet.find();
+        res.status(200).json(foundPets);
+    } catch (err) {
+        res.status(500).json({ err: err.message})    
+    }
+});
+
+// READ - GET /pets/:petId
+router.get('/:petId', async (req, res) => {
+  try {
+    const foundPet = await Pet.findById(req.params.petId);
+    if (!foundPet) {
+      res.status(404);
+      throw new Error('Pet not found.');
+    }
+    res.status(200).json(foundPet);
+  } catch (err) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      // Add else statement to handle all other errors
+      res.status(500).json({ err: err.message });
+    }
+  }
+});
+
+
 module.exports = router;
