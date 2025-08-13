1d8bd724975706852618ba55ec69cd4d6a417f10
mrodavis <orville.davis@scrapit.io>
2025-08-13 11:29:08 -0400

    defined post functionality

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,3 +1,14 @@
 const Pet = require('../models/pet.js');
 const express = require('express');
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const createdPet = await Pet.create(req.body);
+        res.status(201).json(createdPet);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+module.exports = router;
